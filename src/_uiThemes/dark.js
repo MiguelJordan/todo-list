aa95c6d2110243043dd8e45cb775785923e75131
kamtoeddy <lollipopkalog@gmail.com>
2021-12-24 07:59:56 +0100

    modded presence indicator style

--- src/_uiThemes/dark.js
+++ src/_uiThemes/dark.js
@@ -3,15 +3,17 @@ import { createTheme } from "@mui/material/styles";
 export default createTheme({
   palette: {
     background: {
-      paper: "#fff",
+      paper: "#001d42",
     },
     text: {
-      primary: "#173A5E",
-      secondary: "#46505A",
+      primary: "#001d42",
+      secondary: "#hsl(218, 4%, 63%)",
+      light: "white",
     },
     action: {
       active: "#001E3C",
     },
+    indicator: { active: "#4caf50" },
     nav: {
       active: "white",
       inActive: "hsl(218, 4%, 63%)",

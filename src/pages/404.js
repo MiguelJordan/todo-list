fd2f1d076c122c75f53735cadf3403e2ac288407
kamtoeddy <lollipopkalog@gmail.com>
2021-12-27 12:21:32 +0100

    styled and translated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,41 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+
+import { TrContext } from "../contexts/TranslationContext";
+
 import Layout from "../components/_layout/Layout";
 
 const Page = () => {
+  const { t } = useContext(TrContext);
+  const Navigate = useNavigate();
+
   return (
     <>
-      <h1>404</h1>
-      <div>Page Not Found</div>
+      <div style={{ fontSize: "xx-large", marginTop: "20vh" }}>
+        <h1 className="center">404</h1>
+        <div className="center">{t("pages.404.message")}</div>
+      </div>
+      <Button
+        onClick={() => Navigate("/")}
+        sx={{
+          my: 2,
+          backgroundColor: "hsl(216, 100%, 46%)",
+          color: "white",
+          display: "block",
+          border: "3px solid transparent",
+          margin: "10px auto",
+          "&:hover": {
+            border: "3px solid hsl(216, 100%, 46%)",
+          },
+        }}
+      >
+        {t("pages.404.back_home")}
+      </Button>
     </>
   );
 };
 
 export default function NotFound() {
-  return <Layout Main={Page} />;
+  return <Layout Main={Page} nav={false} />;
 }

6e2957a748d2031232a093be5a6276a149b8ccb1
miguel <jordanmiguel505@gmail.com>
2022-01-05 03:57:10 +0100

    redirect waiter after order creation

---
+++ src/pages/waiter/Items.add.js
@@ -0,0 +1,92 @@
+import { useContext, useEffect, useState } from "react";
+
+// components
+import ItemList from "../../components/storeItems/ItemList";
+import Search from "../../components/subComponents/Search";
+import Dropdown from "../../components/subComponents/Dropdown";
+
+// contexts
+// import { AuthContext } from "../../contexts/AuthContext";
+// import { TrContext } from "../../contexts/TranslationContext";
+import { ItemContext } from "../../contexts/ItemContext";
+
+// functions
+import { filter, getList, groupData } from "../../functions/data";
+
+export default function AddItems() {
+  // const { user } = useContext(AuthContext);
+  // const { t } = useContext(TrContext);
+  const { items } = useContext(ItemContext);
+  const [searchVal, setSearchVal] = useState("");
+
+  const [families] = useState(getList({ data: items, criteria: "family" }));
+  const [family, setFam] = useState(families[0] ?? "");
+
+  const data = groupData({ data: items, criteria: "family" });
+
+  const [categories, setCats] = useState(
+    getList({ data: data[family], criteria: "category" })
+  );
+  const [category, setCat] = useState(categories[0]);
+
+  const [_items, setItems] = useState(
+    filter({ data: data[family], criteria: "category", value: category })
+  );
+
+  const filterArray = [];
+
+  _items.filter((val) => {
+    if (!searchVal) return filterArray.push(val);
+    if (val.name.toLowerCase().includes(searchVal.toLowerCase().trim()))
+      return filterArray.push(val);
+    return "";
+  });
+
+  useEffect(() => {
+    setCats(getList({ data: data[family], criteria: "category" }));
+  }, [family]);
+
+  useEffect(() => {
+    setItems(
+      filter({ data: data[family], criteria: "category", value: category })
+    );
+  }, [category]);
+
+  useEffect(() => {
+    setCat(categories[0]);
+  }, [categories]);
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexFlow: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          margin: "15px 0",
+        }}
+      >
+        {/* <span>
+          Family:{" "}
+          <Dropdown
+            values={families}
+            onchange={(value) => setFam(value)}
+            defaultVal={family}
+          />
+        </span> */}
+        <span>
+          Category:{" "}
+          <Dropdown
+            values={categories}
+            onchange={(value) => setCat(value)}
+            defaultVal={""}
+          />
+        </span>
+        <Search onChange={setSearchVal} />
+      </div>
+      <ItemList items={filterArray} role="waiter" preview={false} />
+    </>
+  );
+}

dfe254f8bc7028b5dc6a5c0260174b5600aba2f2
kamtoeddy <lollipopkalog@gmail.com>
2022-01-09 23:30:28 +0100

    modded DateTime format

--- src/components/subComponents/DateTime.js
+++ src/components/subComponents/DateTime.js
@@ -7,8 +7,7 @@ import { TrContext } from "../../contexts/TranslationContext";
 export default function DateTime({ date = new Date(), sx = {} }) {
   const { language } = useContext(TrContext);
 
-  const _format =
-    language == "en" ? "DD/MM/YYYY | h:mm a" : "DD/MM/YYYY | hh:mm";
+  const _format = language == "en" ? "DD/MM/YY | h:mm a" : "DD/MM/YY | hh:mm";
   const _date = dayjs(date).format(_format);
 
   return <span style={{ color: "inherit", ...sx }}>{_date}</span>;

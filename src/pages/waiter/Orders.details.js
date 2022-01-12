07f48199e8da2f1613f6090e0c773262cbd1dde7
kamtoeddy <lollipopkalog@gmail.com>
2022-01-12 15:47:17 +0100

    protected add item page of order against invalid order ids

--- src/pages/waiter/Orders.details.js
+++ src/pages/waiter/Orders.details.js
@@ -1,16 +1,17 @@
 import { useContext } from "react";
-import { Navigate } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 
+// contexts
 import { AuthContext } from "../../contexts/AuthContext";
 import { OrderContext } from "../../contexts/OrderContext";
-import OrderDetails from "../../components/orders/OrderDetails";
 
-import { useParams } from "react-router-dom";
+// components
+import OrderDetails from "../../components/orders/OrderDetails";
 
 export default function OrderDetail() {
   const { user } = useContext(AuthContext);
   const { findOrder } = useContext(OrderContext);
-  let { id } = useParams();
+  const { id } = useParams();
 
   const order = findOrder({ key: "id", value: id });
 

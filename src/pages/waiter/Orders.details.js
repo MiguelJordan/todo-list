3b09d54ab042154cda2b26e57d35e1b9eb2a365a
kamtoeddy <lollipopkalog@gmail.com>
2022-01-12 09:20:07 +0100

    protected order details page against invalid orders

--- src/pages/waiter/Orders.details.js
+++ src/pages/waiter/Orders.details.js
@@ -1,14 +1,20 @@
 import { useContext } from "react";
+import { Navigate } from "react-router-dom";
 
+import { AuthContext } from "../../contexts/AuthContext";
 import { OrderContext } from "../../contexts/OrderContext";
 import OrderDetails from "../../components/orders/OrderDetails";
 
 import { useParams } from "react-router-dom";
 
 export default function OrderDetail() {
+  const { user } = useContext(AuthContext);
   const { findOrder } = useContext(OrderContext);
   let { id } = useParams();
+
   const order = findOrder({ key: "id", value: id });
+
+  if (!order) return <Navigate to={`/${user.role}/orders`} />;
 
   return <OrderDetails role="waiter" items={order?.items ?? []} />;
 }

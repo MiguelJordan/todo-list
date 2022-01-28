0b738000217c732d9bcdaa49d40f284269164ee1
kamtoeddy <lollipopkalog@gmail.com>
2022-01-28 19:24:18 +0100

    modded Items.edit

--- src/pages/admin/Item.edit.js
+++ src/pages/admin/Item.edit.js
@@ -8,7 +8,7 @@ import AmForm from "../../components/subComponents/AmForm";
 import { ItemContext } from "../../contexts/ItemContext";
 
 // functions
-import { findElement, toBase64 } from "../../functions/data";
+import { findElement } from "../../functions/data";
 
 export default function EditItem() {
   const { items } = useContext(ItemContext);
@@ -17,9 +17,7 @@ export default function EditItem() {
 
   const item = findElement({ data: items, key: "id", value: id });
 
-  if (!item) return <Navigate to={"/admin/items"} />;
-
-  return (
+  return item ? (
     <AmForm
       storeItem={item}
       modify={true}
@@ -32,5 +30,7 @@ export default function EditItem() {
       setImage={() => {}}
       setError={() => {}}
     />
+  ) : (
+    <Navigate to={"/admin/items"} />
   );
 }

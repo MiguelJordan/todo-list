97c55bde0858feebdb837441bd5c1d6c17aca352
miguel <jordanmiguel505@gmail.com>
2022-01-28 18:07:18 +0100

    modded admin item-add,item-edit

--- src/pages/admin/Item.add.js
+++ src/pages/admin/Item.add.js
@@ -10,7 +10,7 @@ import { SocketContext } from "../../contexts/SocketContext";
 import { TranslationContext } from "../../contexts/TranslationContext";
 
 // functions
-import { validateItem, AddImage, RemoveImage } from "../../functions/data";
+import { toBase64 } from "../../functions/data";
 import { sendFormData } from "../../functions/http";
 import queries from "../../functions/queries";
 
@@ -25,9 +25,85 @@ export default function StoreAdd() {
   const [image, setImage] = useState(null);
   const [imageUrl, setImageUrl] = useState(null);
 
-  const imageAdd = (e) => AddImage(e, setError, setImage, setImageUrl);
+  const AddImage = async (e) => {
+    if (!e) return "";
+    setError("");
+    let file = e.target.files[0];
 
-  const imageRemove = () => RemoveImage(setImage, setImageUrl);
+    const typeInfo = file.type.split("/"); // [mimeType ,extension]
+
+    // validate type & extension
+    if (
+      typeInfo[0] !== "image" ||
+      !["jpg", "png", "jpeg"].includes(typeInfo[1])
+    ) {
+      return setError("Invalid image - format");
+    }
+
+    // validate file size
+    if (file.size > 5 * 1024 * 1024) {
+      return setError("Invalid image - size too large");
+    }
+
+    let base64 = await toBase64(file);
+
+    setImage(base64);
+    setImageUrl(file);
+  };
+
+  const RemoveImage = () => {
+    setImage(null);
+    setImageUrl(null);
+  };
+
+  const validateItem = (item) => {
+    item.name = item.name?.trim();
+    if (!item.name) return { valid: false, message: "Invalid item name" };
+
+    item.family = item.family?.trim();
+    if (!item.family) return { valid: false, message: "Invalid family" };
+
+    item.category = item.category?.trim();
+    if (!item.category) return { valid: false, message: "Invalid category" };
+
+    item.measureUnit = item.measureUnit?.trim();
+    if (!item.measureUnit) {
+      return { valid: false, message: "Invalid measure unit" };
+    }
+
+    item.measureUnitPlural = item.measureUnitPlural?.trim();
+    if (!item.measureUnitPlural) {
+      return { valid: false, message: "Invalid measure unit" };
+    }
+
+    if (isNaN(item.quantity) || item.quantity < 0) {
+      return { valid: false, message: "Invalid quantity" };
+    }
+    item.quantity = Number(item.quantity);
+
+    if (isNaN(item.cost) || item.cost < 0) {
+      return { valid: false, message: "Invalid cost price" };
+    }
+    item.cost = Number(item.cost);
+
+    if (isNaN(item.commission) || item.commission < 0) {
+      return { valid: false, message: "Invalid commission" };
+    }
+    item.commission = Number(item.commission);
+
+    if (isNaN(item.commissionRatio) || item.commissionRatio < 1) {
+      return { valid: false, message: "Invalid commission ratio" };
+    }
+    item.commissionRatio = Number(item.commissionRatio);
+
+    if (imageUrl) {
+      item.imageUrl = imageUrl;
+    } else {
+      delete item?.imageUrl;
+    }
+
+    return { valid: true, validated: item };
+  };
 
   const handleSubmit = async (item, reset) => {
     setError("");
@@ -81,8 +157,8 @@ export default function StoreAdd() {
       target="storeItems"
       handleSubmit={handleSubmit}
       image={image}
-      AddImage={imageAdd}
-      RemoveImage={imageRemove}
+      AddImage={AddImage}
+      RemoveImage={RemoveImage}
       loading={loading}
       error={error}
     />
